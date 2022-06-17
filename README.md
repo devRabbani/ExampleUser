d9483e072232bc0895eed6199f262b3acc35e944
StrikerOne65 <golamrabbani20014@gmail.com>
2022-06-17 20:34:23 +0530

    Added first files

---
+++ README.md
@@ -0,0 +1,3 @@
+## ExampleUser
+
+It is simple react api fetching example app with added feature like saving user data to localstorage and remove it.
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Content from './Content'
+import './App.css'
+
+function App() {
+  return (
+    <div className='App'>
+      <nav>ExampleUser</nav>
+      <Content />
+    </div>
+  )
+}
+
+export default App
